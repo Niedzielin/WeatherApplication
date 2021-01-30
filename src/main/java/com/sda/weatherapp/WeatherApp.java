63dcb1b717ba0827fe55c3972ed0599c02a93d5a
Pjoter <pitersoncs@gmail.com>
2021-01-30 13:21:41 +0100

    Initial commit

---
+++ src/main/java/com/sda/weatherapp/WeatherApp.java
@@ -0,0 +1,10 @@
+package com.sda.weatherapp;
+
+/**
+ * Created by Hyperbook on 30.01.2021
+ **/
+public class WeatherApp {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/sda/weatherapp/frontend/UserInterface.java
@@ -0,0 +1,20 @@
+package com.sda.weatherapp.frontend;
+
+/**
+ * Created by Hyperbook on 30.01.2021
+ **/
+public class UserInterface {
+    public void runApp(){
+
+    }
+    public void mainMenu(){
+        System.out.println("Welcome in Weather App!");
+        while(true){
+            System.out.println("1. Add location");
+            System.out.println(2.);
+        }
+    }
+    public void addLocMenu(){
+
+    }
+}
