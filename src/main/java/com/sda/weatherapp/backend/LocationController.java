529f8ea8b7c55611799f2c62d91d80d7307bb754
Pjoter <pitersoncs@gmail.com>
2021-01-30 14:04:58 +0100

    x

--- src/main/java/com/sda/weatherapp/backend/LocationController.java
+++ src/main/java/com/sda/weatherapp/backend/LocationController.java
@@ -1,7 +1,12 @@
 package com.sda.weatherapp.backend;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 /**
  * Created by Hyperbook on 30.01.2021
  **/
 public class LocationController {
+    private final ObjectMapper objectMapper;
+    private final LocationService locationService;
+    private final LocationMapper locationMapper;
 }
---
+++ src/main/java/com/sda/weatherapp/backend/LocationService.java
@@ -0,0 +1,7 @@
+package com.sda.weatherapp.backend;
+
+/**
+ * Created by Hyperbook on 30.01.2021
+ **/
+public class LocationService {
+}
